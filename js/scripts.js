32036f6c3bf95605e45ad39fe10df048b66ec591
Jeff Kim <kim2jy@hotmail.com>
2020-06-01 15:57:12 -0700

    add the first three functions of the practice prompt
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 const sentence = prompt("Please enter a sentence");
 
-function firstLastUpper(sentence) {
+// Business Logic
+
+// const string1 = firstLastUpper(sentence);
+// const string2 = reverseFirstLast(string1);
+
+
+function firstLastUpper() {
   const n = sentence.length;
   const firstLast = sentence.charAt(0) + sentence.charAt(n-1);
   const result = firstLast.toUpperCase();
   return result;
 };
 
-alert(firstLastUpper(sentence));
+function reverseFirstLast() {
+  return firstLastUpper(sentence).charAt(1) + firstLastUpper(sentence).charAt(0);
+};
+
+function getLastTwo() {
+  const string1 = firstLastUpper(sentence);
+  const string2 = reverseFirstLast(string1);
+  return string2;
+};
+
+alert(getLastTwo());
